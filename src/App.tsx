ca7614dec433dd0d20d64934956277c2d780675a
bngoon <bookerngoon@gmail.com>
2024-06-06 16:30:40 -0400

    toggle info

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css'
 import { Person } from './Person'
 
+
+
+
+
+
+
+
 function App() {
 
 
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 
 
 
 export const Person = (props) => {
+
+    const [isShowInfo, setShowInfo] = useState<boolean>(true);
+    
+    const toggleInfo = () => {
+        setShowInfo((prev)=> !prev);
+    }
+
     return (
         <div>
+            {isShowInfo && ( 
+            <>
             <p>{props.name}</p>
             <p>{props.age}</p>
             <p>this person{props.isMarried ? " is married" : " is single"}</p>
+            </>
+            )}
+
+            <button onClick={toggleInfo}>Toggle Info</button>
         </div>
     )
 }
